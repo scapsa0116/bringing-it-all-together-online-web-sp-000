70f20874bd9cdd501c9d8d9e2953ebca78213a44
Stela Capsa <stela.capsa@gmail.com>
2020-02-22 20:31:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -74,7 +74,7 @@ def self.new_from_db(row)
   end 
 
 
-def self.find_by_name(name)
+ def self.find_by_name(name)
     sql = <<-SQL
       SELECT * FROM dogs WHERE name = ?
     SQL
@@ -82,7 +82,7 @@ def self.find_by_name(name)
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
-end 
+  end 
 
 
   def update
