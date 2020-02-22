946e807767553320ca04262d383a478bcccab61b
Stela Capsa <stela.capsa@gmail.com>
2020-02-22 19:57:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,6 @@ class Dog
   attr_accessor :id, :name, :breed
 
   def initialize(attributes)
-    #id: nil, name:, breed:
     attributes.each {|key, value| self.send(("#{key}="), value)}
     self.id ||= nil
   end
