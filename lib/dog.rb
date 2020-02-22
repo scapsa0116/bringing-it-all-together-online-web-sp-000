6e4d13ff7b9b4d6cd6625ae41ee724c2890b620e
Stela Capsa <stela.capsa@gmail.com>
2020-02-22 20:02:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,7 +29,7 @@ class Dog
     INSERT INTO dogs (name, breed) VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.breed)
-    @id = 
-  
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  end 
   
 end 
