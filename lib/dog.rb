3d20c93990e5c0c47dcf2a0b9ff23d18975edf47
Stela Capsa <stela.capsa@gmail.com>
2020-02-22 19:56:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,22 +2,22 @@ class Dog
   attr_accessor :id, :name, :breed
 
   def initialize(attributes)
+    #id: nil, name:, breed:
     attributes.each {|key, value| self.send(("#{key}="), value)}
     self.id ||= nil
   end
-  
+
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE IF NOT EXIST dogs(
-    id INTEGER PRYMARY KEY,
-    name TEXT,
-    breed TEXT 
-   )
-    SQL
-    DB[:conn].execute(sql)
-  end 
-  
-  
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
+
+      DB[:conn].execute(sql)
+  end
   
   
   
